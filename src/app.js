45eb7084936f221a92d65bc5ca289c47e023918f
hmarat <marat.hakobjanyan9@gmail.com>
2019-11-04 22:30:05 +0400

    To add styles to header, and install normalize.css

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import IndesicionApp from "./components/IndesicionApp"
 import React from "react"
 import ReactDOM from "react-dom"
+import "normalize.css/normalize.css"
 import "./styles/styles.scss"
 
 ReactDOM.render(<IndesicionApp />, document.getElementById("app")); 
